914771c9aa734861d1000b13515cc0572f03b7da
Senti0027 <5b0g0027@stust.edu.tw>
2023-12-11 22:05:05 +0800

    #ed4n

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -39,7 +39,72 @@ namespace WpfApp2
 
         private void InititalizeCourse()
         {
-            
+            Teacher teacher1 = new Teacher
+            {
+                TeacherName="陳定宏"
+            };
+            teacher1.TeachingCourses.Add(new Course(teacher1) {
+                CourseName = "視窗程式設計",
+                OpeningClass = "五專資工三甲",
+                Type = "必修",
+                Point = 3
+            });
+            teacher1.TeachingCourses.Add(new Course(teacher1) {
+                CourseName = "視窗程式設計",
+                OpeningClass = "四技二甲",
+                Point = 3, Type = "選修" 
+            });
+            teacher1.TeachingCourses.Add(new Course(teacher1) {
+                CourseName = "視窗程式設計",
+                OpeningClass = "四技二乙",
+                Point = 3,
+                Type = "選修" 
+            });
+            teacher1.TeachingCourses.Add(new Course(teacher1) {
+                CourseName = "視窗程式設計", 
+                OpeningClass = "四技二丙", 
+                Point = 3, 
+                Type = "選修" 
+            });
+            Teacher teacher2 = new Teacher() { TeacherName = "陳福坤" };
+            teacher2.TeachingCourses.Add(new Course(teacher2) {
+                CourseName = "計算機概論",
+                OpeningClass = "四技一丙", 
+                Point = 2, 
+                Type = "必修" 
+            });
+            teacher2.TeachingCourses.Add(new Course(teacher2) {
+                CourseName = "計算機概論", 
+                OpeningClass = "四技一甲一乙",
+                Point = 2,
+                Type = "必修" 
+            });
+            teacher2.TeachingCourses.Add(new Course(teacher2) {
+                CourseName = "數位系統導論", 
+                OpeningClass = "四技一乙",
+                Point = 3,
+                Type = "必修" 
+            });
+
+            Teacher teacher3 = new Teacher() { TeacherName = "許子衡" };
+            teacher3.TeachingCourses.Add(new Course(teacher3) { 
+                CourseName = "Android程式設計",
+                OpeningClass = "四技資工三甲等合開",
+                Point = 3, 
+                Type = "選修" 
+            });
+            teacher3.TeachingCourses.Add(new Course(teacher3) {
+                CourseName = "人工智慧與雲端運算",
+                OpeningClass = "四技資工四甲等合開", 
+                Point = 3,
+                Type = "選修" 
+            });
+            teacher3.TeachingCourses.Add(new Course(teacher3) {
+                CourseName = "動態程式語言", 
+                OpeningClass = "五專資工三甲", 
+                Point = 3,
+                Type = "系定選修"
+            });
         }
 
         private void InitializeStudent()
--- WpfApp2/WpfApp2/Teacher.cs
+++ WpfApp2/WpfApp2/Teacher.cs
@@ -10,7 +10,8 @@ namespace WpfApp2
     internal class Teacher
     {
         public String? TeacherName { get; set; }
-        public ObservableCollection<Course> TeachingCourses { get; set; }=new ObservableCollection<Course>();//在更動集合中的項目時發出通知
+        public ObservableCollection<Course> TeachingCourses { get; set; }=
+        new ObservableCollection<Course>();//在更動集合中的項目時發出通知
 
 
     }
