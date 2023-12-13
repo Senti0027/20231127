071052b2fa989a01c2954dfa5b0eebabc3dbf482
Senti0027 <5b0g0027@stust.edu.tw>
2023-12-13 10:38:06 +0800

    #@111

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -43,68 +43,40 @@ namespace WpfApp2
             {
                 TeacherName="陳定宏"
             };
-            teacher1.TeachingCourses.Add(new Course(teacher1) {
-                CourseName = "視窗程式設計",
-                OpeningClass = "五專資工三甲",
-                Type = "必修",
-                Point = 3
-            });
-            teacher1.TeachingCourses.Add(new Course(teacher1) {
-                CourseName = "視窗程式設計",
-                OpeningClass = "四技二甲",
-                Point = 3, Type = "選修" 
-            });
-            teacher1.TeachingCourses.Add(new Course(teacher1) {
-                CourseName = "視窗程式設計",
-                OpeningClass = "四技二乙",
-                Point = 3,
-                Type = "選修" 
-            });
-            teacher1.TeachingCourses.Add(new Course(teacher1) {
-                CourseName = "視窗程式設計", 
-                OpeningClass = "四技二丙", 
-                Point = 3, 
-                Type = "選修" 
-            });
-            Teacher teacher2 = new Teacher() { TeacherName = "陳福坤" };
-            teacher2.TeachingCourses.Add(new Course(teacher2) {
-                CourseName = "計算機概論",
-                OpeningClass = "四技一丙", 
-                Point = 2, 
-                Type = "必修" 
-            });
-            teacher2.TeachingCourses.Add(new Course(teacher2) {
-                CourseName = "計算機概論", 
-                OpeningClass = "四技一甲一乙",
-                Point = 2,
-                Type = "必修" 
-            });
-            teacher2.TeachingCourses.Add(new Course(teacher2) {
-                CourseName = "數位系統導論", 
-                OpeningClass = "四技一乙",
-                Point = 3,
-                Type = "必修" 
-            });
+            teacher1.TeachingCourses.Add(new Course(teacher1) {CourseName = "視窗程式設計", OpeningClass = "五專資工三甲",Type = "必修", Point = 3 });
+            teacher1.TeachingCourses.Add(new Course(teacher1) { CourseName = "視窗程式設計", OpeningClass = "四技二甲", Point = 3, Type = "選修" });
+            teacher1.TeachingCourses.Add(new Course(teacher1) { CourseName = "視窗程式設計", OpeningClass = "四技二乙", Point = 3, Type = "選修" });
+            teacher1.TeachingCourses.Add(new Course(teacher1) { CourseName = "視窗程式設計", OpeningClass = "四技二丙", Point = 3, Type = "選修" });
 
-            Teacher teacher3 = new Teacher() { TeacherName = "許子衡" };
-            teacher3.TeachingCourses.Add(new Course(teacher3) { 
-                CourseName = "Android程式設計",
-                OpeningClass = "四技資工三甲等合開",
-                Point = 3, 
-                Type = "選修" 
-            });
-            teacher3.TeachingCourses.Add(new Course(teacher3) {
-                CourseName = "人工智慧與雲端運算",
-                OpeningClass = "四技資工四甲等合開", 
-                Point = 3,
-                Type = "選修" 
-            });
-            teacher3.TeachingCourses.Add(new Course(teacher3) {
-                CourseName = "動態程式語言", 
-                OpeningClass = "五專資工三甲", 
-                Point = 3,
-                Type = "系定選修"
-            });
+            Teacher teacher2 = new Teacher() 
+            { 
+                TeacherName = "陳福坤"
+            };
+            teacher2.TeachingCourses.Add(new Course(teacher2) { CourseName = "計算機概論",OpeningClass = "四技一丙", Point = 2, Type = "必修" });
+            teacher2.TeachingCourses.Add(new Course(teacher2) { CourseName = "計算機概論", OpeningClass = "四技一丙", Point = 2, Type = "必修" });
+            teacher2.TeachingCourses.Add(new Course(teacher2) { CourseName = "計算機概論", OpeningClass = "四技一甲一乙", Point = 2, Type = "必修" });
+            teacher2.TeachingCourses.Add(new Course(teacher2) { CourseName = "數位系統導論", OpeningClass = "四技一乙", Point = 3, Type = "必修" });
+
+            Teacher teacher3 = new Teacher()
+            { 
+                TeacherName = "許子衡"
+            };
+            teacher3.TeachingCourses.Add(new Course(teacher3) { CourseName = "Android程式設計", OpeningClass = "四技資工三甲等合開",Point = 3, Type = "選修"});
+            teacher3.TeachingCourses.Add(new Course(teacher3) { CourseName = "人工智慧與雲端運算", OpeningClass = "四技資工四甲等合開", Point = 3, Type = "選修" });
+            teacher3.TeachingCourses.Add(new Course(teacher3) { CourseName = "動態程式語言", OpeningClass = "五專資工三甲", Point = 3, Type = "系定選修" });
+            
+            teachers.Add(teacher1);
+            teachers.Add(teacher2);
+            teachers.Add(teacher3);
+            tvTeacher.ItemsSource = teachers;
+            foreach (Teacher teacher in teachers)
+            {
+                foreach (Course course in teacher.TeachingCourses)
+                {
+                    courses.Add(course);
+                }
+            }
+            lbCourse.ItemsSource = courses;
         }
 
         private void InitializeStudent()
@@ -133,6 +105,11 @@ namespace WpfApp2
         {
             selectedStudent=(Student)cmbStudent.SelectedItem;
             LabelStatus.Content = $"選取學生:{selectedStudent.ToString()}";
+        }
+
+        private void lbCourse_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
